4d040a45458279ddf19fac34b85cd63f1a12ab64
TTatchell <tim.tatchell24@gmail.com>
2021-03-17 12:17:58 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    globalThis.bestCustomer = 'maybe bob';
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+const leastFavoriteCustomer = 'john';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'medow';
+}
+
+console.log(changeLeastFavoriteCustomer());
